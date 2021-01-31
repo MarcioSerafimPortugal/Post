e5bd9e1bc91b614f91a882102390389e1fbf06ce
Marcio Serafim de Almeida <marcioserafimportugal@gmail.com>
2021-01-31 14:51:08 +0000

    removido a máquina virtual do projeto, porque é desnecessária e verificado a forma de mostrar os dados do BD em um tempalte

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import hello_word
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello_word),
+    path('blog', include('website.urls')),
 ]
